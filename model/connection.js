77b5fefaecea859aeb09838297e1cccab6361aa3
EdwinDX <you@example.comletiendung502@gmail.com>
2022-07-27 11:34:22 +0700

    local fix

--- model/connection.js
+++ model/connection.js
@@ -6,7 +6,7 @@ class Connection {
       let configToMySQL = {
         host: "localhost",
         user: "root",
-        password: "12345678",
+        password: "123456",
         database: "project3",
         charset: "utf8mb4",
       };
