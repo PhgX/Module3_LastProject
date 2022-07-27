b30796eae006b7a47f4ad3a214a1c63d55ed7f7d
PhgX <phuongbm211@gmail.com>
2022-07-27 11:15:28 +0700

    check done

--- model/connection.js
+++ model/connection.js
@@ -6,7 +6,7 @@ class Connection {
       let configToMySQL = {
         host: "localhost",
         user: "root",
-        password: "nguyenthidung95",
+        password: "12345678",
         database: "project3",
         charset: "utf8mb4",
       };
